47206f21a049b0f908eb7c843d5aafa19b80791c
Lucjan Suski <lucjansuski@gmail.com>
2019-05-30 17:15:13 +0200

    Add xxHash directly since crystal shards do not support submodules

--- src/xxhash.cr
+++ src/xxhash.cr
@@ -1,10 +1,10 @@
-@[Link(ldflags: "#{__DIR__}/ext/xxhash.o")]
+@[Link(ldflags: "#{__DIR__}/xxHash/xxhash.o")]
 lib LibXXHash
   fun hash64 = XXH64(input : UInt8*, size : LibC::SizeT, seed : UInt64) : UInt64
 end
 
 module Xxhash
-  VERSION = "0.1.0"
+  VERSION = "0.1.2"
 
   def self.hash64(text : String)
     input = text.bytes
