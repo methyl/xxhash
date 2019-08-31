c3133c92a7bfbb2bf5054b4117693df76b58bb47
BlackHabanero <51138542+BlackHabanero@users.noreply.github.com>
2019-08-31 23:55:40 +0200

    v0.2.0
    * Adds support for hash32 and hash128
    * Adds `Bytes` hashing for all hash types

--- spec/xxhash_spec.cr
+++ spec/xxhash_spec.cr
@@ -2,7 +2,30 @@ require "spec"
 require "../src/xxhash"
 
 describe Xxhash do
-  it "calculates hash64" do
-    Xxhash.hash64("test hash").should eq(11437837717929986876)
+  context "calculates hash128 with" do
+    it "default seed" do
+      expected_hash = LibXXHash::XXH128_Hash.new(low64: 8783152424331972946, high64: 11833394359751653786)
+      Xxhash.hash128("test hash").should eq(expected_hash)
+    end
+    it "11 as seed" do
+      expected_hash = LibXXHash::XXH128_Hash.new(low64: 2734930085301639998, high64: 422024697275480028)
+      Xxhash.hash128("test hash", 11).should eq(expected_hash)
+    end
+  end
+  context "calculates hash64 with" do
+    it "default seed" do
+      Xxhash.hash64("test hash").should eq(11437837717929986876)
+    end
+    it "12 as seed" do
+      Xxhash.hash64("test hash", 12).should eq(17909425895861273630)
+    end
+  end
+  context "calculates hash32 with" do
+    it "default seed" do
+      Xxhash.hash32("test hash").should eq(814550234)
+    end
+    it "13 as seed" do
+      Xxhash.hash32("test hash", 13).should eq(2647094367)
+    end
   end
 end
--- src/xxhash.cr
+++ src/xxhash.cr
@@ -1,14 +1,40 @@
 @[Link(ldflags: "#{__DIR__}/xxHash/xxhash.o")]
 lib LibXXHash
-  fun hash64 = XXH64(input : UInt8*, size : LibC::SizeT, seed : UInt64) : UInt64
+  fun hash128 = XXH128(input : Void*, size : LibC::SizeT, seed : LibC::ULongLong) : XXH128_Hash
+
+  fun hash64 = XXH64(input : Void*, size : LibC::SizeT, seed : LibC::ULongLong) : LibC::ULongLong
+
+  fun hash32 = XXH32(input : Void*, size : LibC::SizeT, seed : LibC::UInt) : LibC::UInt
+
+  struct XXH128_Hash
+    low64, high64 : LibC::ULongLong
+  end
 end
 
 module Xxhash
-  VERSION = "0.1.2"
+  VERSION = "0.2.0"
 
-  def self.hash64(text : String)
-    input = text.bytes
-    size = text.bytesize
-    LibXXHash.hash64(input, size, 0)
+  def self.hash128(text : String, seed : LibC::ULongLong = 0)
+    hash128(text.to_slice, seed: seed)
+  end
+
+  def self.hash128(slice : Bytes, seed : LibC::ULongLong = 0)
+    LibXXHash.hash128(slice, slice.size, seed)
+  end
+
+  def self.hash64(text : String, seed : LibC::ULongLong = 0)
+    hash64(text.to_slice, seed: seed)
+  end
+
+  def self.hash64(slice : Bytes, seed : LibC::ULongLong = 0)
+    LibXXHash.hash64(slice, slice.size, seed)
+  end
+
+  def self.hash32(text : String, seed : LibC::UInt = 0)
+    hash32(text.to_slice, seed: seed)
+  end
+
+  def self.hash32(slice : Bytes, seed : LibC::UInt = 0)
+    LibXXHash.hash32(slice, slice.size, seed)
   end
 end
